ec0d7a9eb8e3904a2719cf51f4987ad3b1234be9
Adol <adol2280@gmail.com>
2025-09-04 21:40:22 +0600

    Curiosity

---
+++ Cleaner.py
@@ -0,0 +1,28 @@
+import os, shutil
+
+cache_dirs = [
+    r"C:\Windows\Temp",
+    os.path.expandvars(r"C:\Users\%USERNAME%\AppData\Local\Temp"),
+    r"C:\Windows\Prefetch",
+    r"C:\Windows\SoftwareDistribution\Download",
+    os.path.expandvars(r"C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\INetCache"),
+    os.path.expandvars(r"C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Recent"),
+    r"C:\$Recycle.Bin"
+]
+
+
+
+for cache in cache_dirs:
+    if os.path.exists(cache):
+        for item in os.listdir(cache):
+            item_path = os.path.join(cache, item)
+            try:
+                if os.path.isfile(item_path):
+                    os.remove(item_path)
+                elif os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
+            except Exception as e:
+                pass
+            except PermissionError as p:
+                print("l")
+
